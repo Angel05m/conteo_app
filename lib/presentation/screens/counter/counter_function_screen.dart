50a83c902e143c4586deac6ef3a4aaf02022e468
Angel05m <aasm052002@gmail.com>
2026-04-30 10:41:49 -0700

    first commit

---
+++ lib/presentation/screens/counter/counter_function_screen.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class CounterFunctionScreen extends StatefulWidget {
+  const CounterFunctionScreen({super.key});
+
+  @override
+  State<CounterFunctionScreen> createState() => _CounterFunctionScreenState();
+}
+
+class _CounterFunctionScreenState extends State<CounterFunctionScreen> {
+  int clickCounter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Counter Screen'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.refresh_rounded),
+            onPressed: () {
+              setState(() {
+                clickCounter = 0;
+              });
+            },
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '$clickCounter',
+              style: const TextStyle(
+                fontSize: 160,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'Click${clickCounter == 1 ? '' : 's'}',
+              style: const TextStyle(fontSize: 25),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            clickCounter++;
+          });
+        },
+        child: const Icon(Icons.plus_one),
+      ),
+    );
+  }
+}
